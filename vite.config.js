7933df4919c0c07f0968864d9339a42108004827
Akshita Agarwal <akshita.plexoc@gmail@example.com>
2026-08-21 17:17:33 +0530

    Fix GitHub Pages routing

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { copyFileSync } from 'node:fs';
-import { resolve } from 'node:path';
-import { cwd } from 'node:process';
- 
-function copyIndexTo404() {
-  return {
-    name: 'copy-index-to-404',
-    closeBundle() {
-      const distDir = resolve(cwd(), 'dist')
-      copyFileSync(resolve(distDir, 'index.html'), resolve(distDir, '404.html'))
-    },
-  }
-}
+
 export default defineConfig({
- plugins: [react(), copyIndexTo404()],
+  plugins: [react()],
   base: "/todo-app/",
 });
